5f7dff52f44ff17f84c871ab7c1e44f6245253aa
Hyacinthe Kouassi <hyacinthe.kouassi@gmail.com>
2013-07-10 01:04:43 +0000

    Modify web.js to read from file

--- web.js
+++ web.js
@@ -1,5 +1,5 @@
-
-var fs=require ('fs');
+#!/usr/bin/env node
+var fs=require('fs');
 var buf=new Buffer(100);
- buf=fs.readFileSync('index.html');
+ buf=fs.readFileSync('./index.html');
 buf.toString("utf-8");

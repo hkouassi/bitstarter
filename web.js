dc9496548607d4aaf9deb5fac7985d00309df9dd
Hyacinthe Kouassi <hyacinthe.kouassi@gmail.com>
2013-07-10 01:39:31 +0000

    Modify web.js to read from file

--- web.js
+++ web.js
@@ -1,5 +1,20 @@
-#!/usr/bin/env node
-var fs=require('fs');
-var buf=new Buffer(100);
- buf=fs.readFileSync('index.html');
-console.log(buf.toString('utf-8'));
+
+
+#!/usr/bin/env node                                                                                  
+var fs=require('fs');                                                                                
+var buf=new Buffer(100);                                                                             
+ buf=fs.readFileSync('index.html');                                                                  
+ 
+
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send(buf);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
